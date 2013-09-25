0a04c056deeefadb8d468e5f1c6f505524105a14
MasterGos <mastergos@wp.pl>
2013-09-25 20:38:14 +0200

    Initialization new maven projects

---
+++ workspace/Agent-BasedLoadBalancer/Client/src/main/java/pl/jgoslawski/Main.java
@@ -0,0 +1,9 @@
+package pl.jgoslawski;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		System.out.println("Test");
+	}
+
+}
---
+++ workspace/Agent-BasedLoadBalancer/Forwarder/src/main/java/pl/jgoslawski/Main.java
@@ -0,0 +1,10 @@
+package pl.jgoslawski;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
